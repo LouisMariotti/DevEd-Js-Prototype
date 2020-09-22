c7154767c059cfb8b80fcab40a31891069f07efb
LouisMariotti <68160210+LouisMariotti@users.noreply.github.com>
2020-09-22 15:26:41 +0200

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,27 @@
+// OOP Object oriented programming, other way of writting code
+/* Focuses on creating oobjects with methods on it */
+
+// Constructor function
+/* Its a function that will generate us an object */
+
+// function Todo(name, completed) {
+//   this.name = name;
+//   this.completed = completed;
+// }
+
+// Todo.prototype.getTodoName = function () {
+//   console.log(this.name);
+// };
+
+// const todo = new Todo("Louis", false);
+// const todo1 = new Todo("Valérie", true);
+// todo.getTodoName();
+// console.log(todo, todo1);
+
+// new keywork is going to create a new empty object, we're adding property
+
+const name = "Louis";
+
+const objName = new String("fish");
+
+console.log(objName);
